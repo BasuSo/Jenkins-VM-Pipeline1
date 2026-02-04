f8a8945edadb4973d6fb783c53c32e19b496c563
SOumya <soumyabasu6027@gmail.com>
2026-02-04 14:42:54 +0530

    main.tf changed

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ resource "azurerm_linux_virtual_machine" "MyfirstJenkinsVM" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("C:/Users/rashn/.ssh/id_rsa.pub")
+    public_key = file(".ssh/id_rsa.pub")
   }
 
   os_disk {
